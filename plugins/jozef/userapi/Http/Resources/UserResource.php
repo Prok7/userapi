eed3b2353dbb58ab80966fd435c01b52b32d3cd8
Prok7 <jozeflazur153@gmail.com>
2022-09-03 20:52:52 +0200

    Added updating user, showing user info and deleting

---
+++ plugins/jozef/userapi/Http/Resources/UserResource.php
@@ -0,0 +1,16 @@
+<?php
+    namespace Jozef\Userapi\Http\Resources;
+    use Illuminate\Http\Resources\Json\JsonResource;
+
+    class UserResource extends JsonResource {
+        public function toArray($request) {
+            return [
+                "id" => $this->id,
+                "name" => $this->name,
+                "surname" => $this->surname,
+                "email" => $this->email,
+                "is_activated" => $this->is_activated,
+                "registered" => date($this->created_at)
+            ];
+        }
+    }
--- plugins/jozef/userapi/routes.php
+++ plugins/jozef/userapi/routes.php
@@ -1,4 +1,7 @@
 <?php
+    use RainLab\User\Models\User;
+    use Jozef\Userapi\Http\Resources\UserResource;
+
     // register new user
     Route::post("api/register", function() {
         $name = post("name");
@@ -37,4 +40,45 @@
         $user->password_confirmation = post("new_password_confirmation");
         $user->save();
         return "Password resetted successfully";
+    });
+
+    // update user info
+    Route::post("api/update/user", function() {
+        $user = Auth::authenticate([
+            "email" => post("email"),
+            "password" => post("password")
+        ]);
+
+        $changed = [];
+        $user_info = ["email", "name", "surname"];
+        foreach ($user_info as $info) {
+            if (post("new_$info")) {
+                $user->$info = post("new_$info");
+                array_push($changed, $info);
+            };
+        };
+
+        if (count($changed) > 0) {
+            $user->save();
+            return "Info has been successfully changed";
+        } else {
+            return "No info has been changed";
+        }
+    });
+
+    // show user info
+    Route::post("api/show/user", function() {
+        $user = User::find(post("id"));
+        return new UserResource($user);
+    });
+
+    // delete user
+    Route::post("api/delete/user", function() {
+        $user = Auth::authenticate([
+            "email" => post("email"),
+            "password" => post("password")
+        ]);
+    
+        $user->delete();
+        return "User deactivated";
     });
